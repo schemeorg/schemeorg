e0c41a6adcc108ba4ffa649fd8baf4d38c262f01
Arthur A. Gleckler <aag@alum.mit.edu>
2020-11-26 22:44:49 -0800

    Ignore Emacs backup files.

--- wwwstaging.sh
+++ wwwstaging.sh
@@ -3,4 +3,4 @@ set -eu
 cd "$(dirname "$0")"
 echo "Entering directory '$PWD'"
 set -x
-rsync -vax --delete www/ stag-www@scheme.org:www/
+rsync -vax --delete www/ --exclude="*~" stag-www@scheme.org:www/
