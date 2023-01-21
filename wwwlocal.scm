4679e38735e664fd2663c121f028ab436801ced2
Arthur A. Gleckler <aag@alum.mit.edu>
2023-01-20 21:09:54 -0800

    Serve only scheme.org.
    Schemers.org can be done entirely using Nginx rewrites.

--- wwwlocal.scm
+++ wwwlocal.scm
@@ -1,4 +1,4 @@
-;; Serve static files from the `scheme.org/www` subdirectory over HTTP.
+;; Serve static files from the `www` subdirectory over HTTP.
 ;;
 ;; This is so you can browse the site on localhost using the same URL
 ;; layout that it has on www.scheme.org. We set the bind address to
@@ -13,11 +13,11 @@
 (define (disp . xs) (for-each display xs) (newline))
 
 (define (main)
-  (root-path "scheme.org/www")  ; Serve static files from this directory.
+  (root-path "www")  ; Serve static files from this directory.
   (server-bind-address "localhost")
   (let ((port (get-environment-variable "PORT")))
     (when port (server-port (string->number port))))
-  (disp "Serving contents of scheme.org/www/ directory at "
+  (disp "Serving contents of www/ directory at "
         "http://" (server-bind-address) ":" (server-port) "/")
   (start-server)
   0)
--- wwwproduction.sh
+++ wwwproduction.sh
@@ -3,4 +3,4 @@ set -eu
 cd "$(dirname "$0")"
 echo "Entering directory '$PWD'"
 set -x
-rsync -crv --delete --exclude="*~" scheme.org/www/ scheme.org:/production/www/www/
+rsync -crv --delete --exclude="*~" www/ scheme.org:/production/www/www/
