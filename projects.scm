40e3ec1c49346fef585db140df6c98db5c401ea1
Lassi Kortela <lassi@lassi.io>
2023-01-25 00:30:17 +0200

    Edit conservatory tagline

--- projects.scm
+++ projects.scm
@@ -438,7 +438,7 @@
 
    ((project-id "conservatory")
     (title "Conservatory")
-    (tagline "Community maintenance of old implementations")
+    (tagline "Preservation old software and websites")
     (sidenote "")
     (contacts "lassi")
     (display? #t)
