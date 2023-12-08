2db142a68401bef557d0445d8b133ba3b4cb8eda
Lassi Kortela <lassi@lassi.io>
2023-12-08 12:44:16 +0200

    Add server configuration
    It covers the scheme.org origin server. The other servers are still
    covered by https://github.com/schemeorg-community/linux-configuration

---
+++ lib/schemeorglib.sld
@@ -0,0 +1,141 @@
+(define-library (schemeorglib)
+  (export
+   all-projects
+   append-map
+   circular-generator
+   echo
+   filter
+   filter-map
+   find
+   first
+   flatten-all
+   fold
+   generator->list
+   get-boolean
+   get-list
+   get-string
+   get-string?
+   list-sort
+   pretty-print
+   project-groups
+   projects-data
+   script-directory
+   second
+   sort-unique
+   string-join
+   take
+   third)
+  (import (scheme base)
+          (scheme file)
+          (scheme read)
+          (scheme write)
+          (only (srfi 1)
+                append-map filter filter-map find first fold
+                second take third)
+          (only (srfi 132) list-delete-neighbor-dups)
+          (only (srfi 193) script-directory))
+  (cond-expand
+   ((library (srfi 166))
+    (import (only (srfi 166) pretty show)))
+   (else))
+  (cond-expand
+   ((library (srfi 166))
+    (begin
+      (define (pretty-print x)
+        (show #t (pretty x)))))
+   (else
+    (begin
+      (define (pretty-print x)
+        (write x)
+        (newline)))))
+  (begin
+
+    (define (circular-generator . elements)  ; Specified in SRFI 158.
+      (let ((xs elements))
+        (lambda ()
+          (let ((x (car xs)))
+            (set! xs (if (null? (cdr xs)) elements (cdr xs)))
+            x))))
+
+    (define (generator->list g)  ; Subset of the behavior in SRFI 158.
+      (let loop ((xs '()))
+        (let ((x (g)))
+          (if (eof-object? x) (reverse xs) (loop (cons x xs))))))
+
+    (define (flatten-all x)
+      (if (list? x)
+          (append-map flatten-all x)
+          (list x)))
+
+    (define (list-sort < xs)  ; SRFI 132
+      (define (insert x xs)
+        (if (null? xs) (list x)
+            (if (< x (car xs))
+                (cons x xs)
+                (cons (car xs)
+                      (insert x (cdr xs))))))
+      (let loop ((xs xs))
+        (if (null? xs)
+            '()
+            (insert (car xs)
+                    (loop (cdr xs))))))
+
+    (define (sort-unique strings)
+      (list-delete-neighbor-dups string=? (list-sort string<? strings)))
+
+    ;;
+
+    (define (string-join strings delimiter)
+      (if (null? strings)
+          ""
+          (fold (lambda (s so-far) (string-append so-far delimiter s))
+                (car strings)
+                (cdr strings))))
+
+    ;;
+
+    (define (echo . xs)
+      (for-each display xs)
+      (newline))
+
+    ;;
+
+    (define (get-list key alist)
+      (let ((entry (assoc key alist)))
+        (if entry (cdr entry) '())))
+
+    (define (get-value? key alist valid? not-found)
+      (let ((entry (assoc key alist)))
+        (cond ((not entry)
+               (not-found))
+              ((and (= 2 (length entry))
+                    (valid? (second entry)))
+               (second entry))
+              (else
+               (error "Not valid" entry)))))
+
+    (define (get-value key alist valid?)
+      (get-value? key alist valid? (lambda () (error "Not found" key alist))))
+
+    (define (get-string? key alist)
+      (get-value? key alist string? (lambda () #f)))
+
+    (define (get-string key alist)
+      (get-value key alist string?))
+
+    (define (get-boolean key alist)
+      (get-value key alist boolean?))
+
+    ;;
+
+    (define (projects-file)
+      (string-append (script-directory) "../projects.scm"))
+
+    (define (projects-data)
+      (call-with-input-file (projects-file) read))
+
+    (define (project-groups)
+      (get-list 'projects (projects-data)))
+
+    (define (all-projects)
+      (append-map cdr (get-list 'projects (projects-data))))))
---
+++ lib/sensible.scm
@@ -0,0 +1,326 @@
+(import (scheme base)
+        (scheme file)
+        (schemeorglib))
+
+(define human-users
+  '((0 "lassi" "Lassi Kortela")
+    (1 "arthur" "Arthur Gleckler")))
+
+(define human-user-ordinal first)
+(define human-user-name second)
+(define human-user-display-name third)
+
+(define (human-user-id human-user)
+  (+ 1000 (human-user-ordinal human-user)))
+
+(define (human-user-groups human-user)
+  '("users" "sudo" "docker"))
+
+(define (ssh-key-tasks* user-name human-user-name)
+  (map (lambda (key)
+         `(task
+           (title ,(string-append "add key for " human-user-name))
+           (authorized-key
+            (user ,user-name)
+            (key ,(string-append key " " human-user-name)))))
+       (with-input-from-file (string-append (script-directory)
+                                            "../server/keys/"
+                                            human-user-name)
+         (lambda () (generator->list read-line)))))
+
+(define (ssh-key-tasks user-name)
+  (ssh-key-tasks* user-name user-name))
+
+(define (human-user-tasks human-user)
+  `((task
+     (title ,(string-append "make user " (human-user-name human-user)))
+     (user
+      (uid ,(human-user-id human-user))
+      (name ,(human-user-name human-user))
+      (comment ,(human-user-display-name human-user))
+      (group ,(first (human-user-groups human-user)))
+      (groups ,(cdr (human-user-groups human-user)))
+      (shell "/bin/bash")))
+    ,@(ssh-key-tasks (human-user-name human-user))))
+
+(define (site-tasks subdomain)
+  (let ((site-home (string-append "/var/www/" subdomain)))
+    `((task
+       (title "chmod home dir")
+       (file
+        (path ,site-home)
+        (state "directory")
+        (mode "u=rwX,g=rX,o=rX")
+        (follow false)
+        (recurse false)))
+      (task
+       (title "chown home dir")
+       (file
+        (path ,site-home)
+        (state "directory")
+        (owner "www-data")
+        (group "users")
+        (mode "u=rwX,g=rwX,o=rX")
+        (follow false)
+        (recurse true))))))
+
+(define (write-top-level-expressions . exps)
+  (for-each pretty-print exps))
+
+(write-top-level-expressions
+
+ `(options
+   (var pipelining true))
+
+ `(groups
+   (group
+    (name schemeorg)
+    (hosts
+     (host
+      (name scheme)
+      (vars
+       ;; (var ansible-user "root")
+       (var ansible-host "8.9.4.141")
+       (var ansible-python-interpreter "/usr/bin/python3"))))))
+
+ `(playbooks
+
+   (playbook
+    (name schemeorg)
+    (hosts schemeorg)
+    (become true)
+    (roles
+     apt-upgrade
+     apt-comfort
+     hostname
+     sudo
+     firewall
+     docker
+     human-users
+     site-www
+     site-www-staging
+     site-schemers
+     site-redirect
+     site-files
+     site-lists
+     nginx
+     sshd)))
+
+ `(roles
+
+   (role
+    (name apt-upgrade)
+    (tasks
+     (task
+      (title "upgrade all apt packages to latest versions")
+      (apt
+       (update-cache true)
+       (upgrade true)))))
+
+   (role
+    (name apt-comfort)
+    (tasks
+     (task
+      (title "text editors")
+      (apt
+       (name
+        ("emacs-nox"
+         "mg"
+         "nano"
+         "vim"))))
+     (task
+      (title "other tools")
+      (apt
+       (name
+        ("curl"
+         "dnsutils"
+         "git"
+         "htop"
+         "httpie"
+         "jq"
+         "rsync"
+         "silversearcher-ag"
+         "tmux"
+         "tree"
+         "unzip"
+         "wget"))))))
+
+   (role
+    (name hostname)
+    (tasks
+     (task
+      (title "set hostname")
+      (hostname (name "scheme.org")))))
+
+   (role
+    (name sudo)
+    (tasks
+     (task
+      (title "install sudo")
+      (apt (name "sudo")))
+     (task
+      (title "enable passwordless sudo")
+      (lineinfile
+       (validate "visudo -cqf %s")
+       (path "/etc/sudoers")
+       (regexp "%sudo")
+       (line "%sudo ALL=(ALL:ALL) NOPASSWD:ALL")))
+     (task
+      (title "do not create .sudo_as_admin_successful")
+      (lineinfile
+       (validate "visudo -cqf %s")
+       (path "/etc/sudoers")
+       (line "Defaults !admin_flag")))))
+
+   (role
+    (name sshd)
+    (tasks
+     (task
+      (title "deny ssh password authentication")
+      (lineinfile
+       (dest "/etc/ssh/sshd_config")
+       (regexp "^#?PasswordAuthentication")
+       (line "PasswordAuthentication no"))
+      (notify "restart ssh")))
+    (handlers
+     (handler
+      (title "restart ssh")
+      (service
+       (name "sshd")
+       (state "restarted")))))
+
+   (role
+    (name firewall)
+    (tasks
+     (task
+      (title "install ufw")
+      (apt (name "ufw")))
+     (task
+      (title "allow ssh")
+      (ufw
+       (rule allow)
+       (proto tcp)
+       (port ssh)))
+     (task
+      (title "allow http")
+      (ufw
+       (rule allow)
+       (proto tcp)
+       (port http)))
+     (task
+      (title "allow https")
+      (ufw
+       (rule allow)
+       (proto tcp)
+       (port https)))
+     (task
+      (title "enable firewall with default policy to deny all")
+      (ufw
+       (state enabled)
+       (policy deny)))))
+
+   (role
+    (name docker)
+    (tasks
+     (task
+      (title "install docker")
+      (apt
+       (name
+        ("docker.io"
+         "docker-compose"))))))
+
+   (role
+    (name human-users)
+    (tasks
+     ,@(append-map human-user-tasks
+                   human-users)))
+
+   (role
+    (name site-www)
+    (tasks ,@(site-tasks "www.scheme.org")))
+
+   (role
+    (name site-www-staging)
+    (tasks ,@(site-tasks "www.staging.scheme.org")))
+
+   (role
+    (name site-schemers)
+    (tasks ,@(site-tasks "www.schemers.org")))
+
+   (role
+    (name site-redirect)
+    (tasks ,@(site-tasks "redirect.scheme.org")))
+
+   (role
+    (name site-lists)
+    (tasks ,@(site-tasks "lists.scheme.org")))
+
+   (role
+    (name site-files)
+    (tasks
+     (task
+      (title "symlink")
+      (file
+       (path "/var/www/files.scheme.org")
+       (state "link")
+       (src "/blockstorage/www/files.scheme.org")))
+     (task
+      (title "chmod home dir")
+      (file
+       (path "/blockstorage/www/files.scheme.org")
+       (state "directory")
+       (mode "u=rwX,g=rX,o=rX")
+       (follow false)
+       (recurse false)))
+     (task
+      (title "chown home dir")
+      (file
+       (path "/blockstorage/www/files.scheme.org")
+       (state "directory")
+       (owner "www-data")
+       (group "users")
+       (mode "u=rwX,g=rwX,o=rX")
+       (follow false)
+       (recurse true)))))
+
+   (role
+    (name nginx)
+    (tasks
+     (task
+      (title "install nginx and certbot")
+      (apt
+       (name
+        ("nginx"
+         "certbot"
+         "python3-certbot-nginx")))
+      (notify "restart nginx"))
+     (task
+      (title "backup system nginx.conf to nginx.conf.orig")
+      (copy
+       (dest "/etc/nginx/nginx.conf.orig")
+       (src "/etc/nginx/nginx.conf")
+       (remote-src true)
+       (mode "0444")
+       (force false)))
+     (task
+      (title "install our own nginx.conf")
+      (copy
+       (validate "nginx -t -c %s")
+       (dest "/etc/nginx/nginx.conf")
+       (src "nginx.conf")
+       (owner "root")
+       (group "root")
+       (mode "0644"))
+      (notify "restart nginx"))
+     (task
+      (title "start nginx now and at every boot")
+      (service
+       (name "nginx")
+       (enabled true)
+       (state "started"))))
+    (handlers
+     (handler
+      (title "restart nginx")
+      (service
+       (name "nginx")
+       (state "restarted")))))))
